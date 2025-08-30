fedd47c7d30bcc988260ae0eee59672b96ae264a
GustavoBrast <a2024954370@teiacoltec.org>
2025-08-30 12:12:46 -0300

    Update DadosDoSistema.java

--- Arquivos com Algumas Alterações de Senha, Login e Autenticação/DadosDoSistema.java
+++ Arquivos com Algumas Alterações de Senha, Login e Autenticação/DadosDoSistema.java
@@ -1,1 +1,41 @@
+package org.teiacoltec.poo.tp2;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DadosDoSistema implements Serializable {
+    private List<Turma> listaDeTurmas;
+    private List<Pessoa> listaDePessoas;
+    private List<Atividade> listaDeAtividades;
+    private List<Tarefa> listaDeTarefas;
+    private int proximoIdTarefa;
+
+    public DadosDoSistema(List<Turma> turmas, List<Pessoa> pessoas, List<Atividade> atividades, List<Tarefa> tarefas, int proximoId) {
+        this.listaDeTurmas = turmas;
+        this.listaDePessoas = pessoas;
+        this.listaDeAtividades = atividades;
+        this.listaDeTarefas = tarefas;
+        this.proximoIdTarefa = proximoId;
+    }
+
+    public List<Turma> getListaDeTurmas() {
+        return listaDeTurmas;
+    }
+
+    public List<Pessoa> getListaDePessoas() {
+        return listaDePessoas;
+    }
+
+    public List<Atividade> getListaDeAtividades() {
+        return listaDeAtividades;
+    }
+
+    public List<Tarefa> getListaDeTarefas() {
+        return listaDeTarefas;
+    }
+
+    public int getProximoIdTarefa() {
+        return proximoIdTarefa;
+    }
+}
